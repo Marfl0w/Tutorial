7448420594dde25d5e9d89eb34603af64af22a2f
ulikreznik <26331185-ulikreznik@users.noreply.replit.com>
2023-10-05 15:21:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+      double a,b;
+cout<<("Введите 2 числа. \n");
+cin >> a >> b;
+cout << "сумма = " << a+b << endl;
+cout << "разница = " << a-b << endl;
+} 
