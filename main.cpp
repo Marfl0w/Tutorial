d44935db84342471217f43f1f4133fcd07816c5a
Marfl0w <145397165+Marfl0w@users.noreply.github.com>
2023-10-05 15:31:36 +0000

    Fix bags

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
       double a,b;
-cout<<("Введите 2 числа. \n");
+cout<<("Введите 2 числа: \n");
 cin >> a >> b;
 cout << "сумма = " << a+b << endl;
 cout << "разница = " << a-b << endl;
